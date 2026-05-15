80cccb20fcd1945c299739c8753e22ad9546a4ac
Rahul Kulthia <rahulkulthia8@gmail.com>
2026-05-15 15:13:52 +0530

    fixed vercel build

--- server/server.js
+++ server/server.js
@@ -24,13 +24,12 @@ const app = express();
 
 /* Middleware */
 app.use(express.json());
-
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    credentials: true
+    origin: "https://quiz-master-dun.vercel.app",
+    credentials: true,
   })
-);
+);;
 
 /* Session */
 app.use(
