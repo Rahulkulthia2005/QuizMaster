1ec5bd241a6b1b5b8d4f94aa49bb761a77fc2668
Rahul Kulthia <rahulkulthia8@gmail.com>
2026-05-16 12:58:50 +0530

    fixed vercel build

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,10 +9,15 @@ const otpStore = {};
 
 /* Mail Transport */
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
